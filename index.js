860435989c24842d2b470dd227a80b81f72402e8
petercodesrblx <127464089+petercodesrblx@users.noreply.github.com>
2024-02-13 17:49:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = [".gg/kz72shcwAC","nitro & more","join petal mart!"];
 
 
 let currentIndex = 0;
